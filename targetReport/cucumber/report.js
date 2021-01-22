6283683583d07ae48086bbcd8b79d0314fd9e331
Irene Loughran <BD.Loughran@yahoo.com>
2021-01-22 15:11:02 -0600

    Initial Commit

---
+++ targetReport/cucumber/report.js
@@ -0,0 +1,185 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login into stackoverflow website",
+  "description": "I want to use this template to validate an existing user with their credentials",
+  "id": "login-into-stackoverflow-website",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 8378364755,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Login into stackoverflow website with valid credentials",
+  "description": "",
+  "id": "login-into-stackoverflow-website;login-into-stackoverflow-website-with-valid-credentials",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "User navigates to stackoverflow website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User clicks on Log in button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "User enters a valid username",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User enters a valid password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User clicks on the Submit button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "User is taken to login page successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStep.user_navigates_to_stackoveflow_website()"
+});
+formatter.result({
+  "duration": 2354106863,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_clicks_on_Log_in_button()"
+});
+formatter.result({
+  "duration": 764735604,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_enters_a_valid_username()"
+});
+formatter.result({
+  "duration": 115157975,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_enters_a_valid_password()"
+});
+formatter.result({
+  "duration": 146227085,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_clicks_on_the_Submit_button()"
+});
+formatter.result({
+  "duration": 1743788389,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_is_taken_to_login_page_successfully()"
+});
+formatter.result({
+  "duration": 3051835536,
+  "status": "passed"
+});
+formatter.uri("Login2.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login into stackoverflow website2",
+  "description": "I want to use this template to validate an existing user with their credentials2",
+  "id": "login-into-stackoverflow-website2",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 7111373218,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Login into stackoverflow website with valid credentials2",
+  "description": "",
+  "id": "login-into-stackoverflow-website2;login-into-stackoverflow-website-with-valid-credentials2",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "User navigates to stackoverflow website2",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User clicks on Log in button2",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "User enters a valid username2",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User enters a valid password2",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User clicks on the Submit button2",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "User is taken to login page successfully2",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStep.user_navigates_to_stackoveflow_website2()"
+});
+formatter.result({
+  "duration": 399081,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_clicks_on_Log_in_button2()"
+});
+formatter.result({
+  "duration": 147632,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_enters_a_valid_username2()"
+});
+formatter.result({
+  "duration": 157106,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_enters_a_valid_password2()"
+});
+formatter.result({
+  "duration": 141711,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_clicks_on_the_Submit_button2()"
+});
+formatter.result({
+  "duration": 135396,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.user_is_taken_to_login_page_successfully2()"
+});
+formatter.result({
+  "duration": 3028551861,
+  "status": "passed"
+});
+});
